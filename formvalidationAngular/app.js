9693ac6bc26adf7b8d25b2623152ef2c820acc4a
amp0024 <amp0024@gmail.com>
2016-03-31 12:31:32 -0600

    form validation

---
+++ formvalidationAngular/app.js
@@ -0,0 +1,10 @@
+//javascript list of dependencies. list of modules that my app is dependent on.
+var myApp = angular.module('myApp', ['ngMessages']);
+
+myApp.controller('mainController', function($scope, $log, $filter){
+
+	$scope.name = 'John';
+	$scope.form
+
+
+});
